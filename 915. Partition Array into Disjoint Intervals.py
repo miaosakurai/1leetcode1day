5e1418f48a9f494151654a106a9a386b3455cad4
miaosakurai <867616074@qq.com>
2021-07-23 15:15:34 +0900

    最近在看突如其来的假期

--- 915. Partition Array into Disjoint Intervals.py
+++ 915. Partition Array into Disjoint Intervals.py
@@ -17,3 +17,19 @@ class Solution:
         for i in range(1, n):
             if l_max[i-1]<=r_min[i]:
                 return i
+
+    def partitionDisjoint2(self, nums: List[int]) -> int:
+        l_maxs = [0] * len(nums) 
+        r_mins = [0] * len(nums)
+        
+        l_maxs[0] = nums[0]
+        r_mins[-1] = nums[-1]
+        
+        for i in reversed(range(len(nums)-1)):
+            r_mins[i] = min(r_mins[i+1], nums[i])
+        
+        for i in range(len(nums)-1):
+            l_maxs[i] = max(l_maxs[i-1], nums[i])
+            if l_maxs[i]<=r_mins[i+1]:
+                return i+1
+        
