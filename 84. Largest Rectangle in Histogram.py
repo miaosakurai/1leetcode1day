0f2ebc13b58f0544664c44877550788a6972b080
miaosakurai <867616074@qq.com>
2021-10-05 12:00:52 +0900

    一道经典hard题

--- 84. Largest Rectangle in Histogram.py
+++ 84. Largest Rectangle in Histogram.py
@@ -3,17 +3,14 @@ class Solution:
         stack = []
         res = 0
         for i in range(len(heights)):
-            if len(stack)==0 or heights[i]>=heights[stack[-1]]:
-                stack.append(i)
-            else:
-                while len(stack)!=0 and heights[i]<heights[stack[-1]]:
-                    j = stack.pop()
-                    if len(stack)==0:
-                        cur = heights[j] * (i)
-                    else:
-                        cur = heights[j] * (i-stack[-1]-1)
-                    res = max(res, cur)
-                stack.append(i)
+            while len(stack)!=0 and heights[i]<heights[stack[-1]]:
+                j = stack.pop()
+                if len(stack)==0:
+                    cur = heights[j] * (i)
+                else:
+                    cur = heights[j] * (i-stack[-1]-1)
+                res = max(res, cur)
+            stack.append(i)
                 
         while len(stack)>0:
             j = stack.pop()
