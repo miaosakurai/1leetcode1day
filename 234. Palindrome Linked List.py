f4adf83d6bfc569453fba3646497e45336e46e31
miaosakurai <867616074@qq.com>
2021-11-14 22:32:12 +0900

    冬天就想窝在角落里

--- 234. Palindrome Linked List.py
+++ 234. Palindrome Linked List.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # idea: fast-slow points
+        # idea: fast-slow points, O(n) time, O(n) space
         l = []
         if not head or not head.next:
             return True
@@ -20,5 +20,30 @@ class Solution:
             if l.pop()!=slow.val:
                 return False
             slow = slow.next
+        return True
+        
+
+    def isPalindrome_2(self, head: Optional[ListNode]) -> bool:
+        # idea: fast-slow points, reverse list, O(n) time, O(1) space
+        if not head or not head.next:
+            return True
+        fast, slow, s_pre, s_next = head, head, None, head.next
+        while fast and fast.next:
+            fast = fast.next.next
+            s_next = slow.next
+            slow.next = s_pre
+            s_pre = slow
+            slow = s_next
+
+        if fast and not fast.next:  # 奇数
+            l, r = s_pre, slow.next
+        else: # 偶数
+            l, r = s_pre, slow
+
+        while l:
+            if l.val != r.val:
+                return False
+            l = l.next
+            r = r.next
         return True
         
