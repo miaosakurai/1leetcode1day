6d54c57be8d410e2516168093db73ac846e244af
miaosakurai <867616074@qq.com>
2021-12-01 17:37:47 +0900

    好像有点感冒,精神也不太好

---
+++ 376. Wiggle Subsequence.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums)==1: return 1
+        res = 1
+        inc = True
+        start = 0
+        for i in range(1, len(nums)):
+            if nums[i]==nums[i-1]:
+                continue
+            else:
+                if nums[i]<nums[i-1]:
+                    inc = False
+                else:
+                    inc = True
+                res +=1
+                start = i
+                break
+        
+        for i in range(start+1, len(nums)):
+            if (nums[i]<nums[i-1] and inc) or (nums[i]>nums[i-1] and not inc):
+                res +=1
+                inc = not inc
+        return res
+            
