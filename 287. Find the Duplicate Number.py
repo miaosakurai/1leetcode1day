b08146e330c191fbd8643f0a43026995336cb1d2
miaosakurai <867616074@qq.com>
2022-01-02 13:14:32 +0900

    新年快乐

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -26,7 +26,7 @@ class Solution:
 
     def findDuplicate_hash(self, nums: List[int]) -> int:
         # Hashmap：把每个数字map到相应的index，这样相同的数字就会遇到
-        # # T: O(n), S: O(n) -> 递归最多可能同时产生n次函数调用
+        # T: O(n), S: O(n) -> 递归最多可能同时产生n次函数调用
         def store(nums, cur):
             if nums[cur] == cur:
                 return cur
@@ -37,8 +37,10 @@ class Solution:
         return store(nums, 0)
     
     def findDuplicate_hash2(self, nums: List[int]) -> int:
-        # Hash map
+        # Hashmap不使用递归
+        # T: O(n), S: O(1)
         while nums[0] != nums[nums[0]]:
             nums[nums[0]], nums[0] = nums[0], nums[nums[0]]
+            # 这里要注意交换顺序不能反过来：如果是先赋值nums[0]，那么nums[nums[0]]中的nums[0]会使用修改后的值
         return nums[0]
        
