f56c6cf508bb765dd3d44fd4d2aefc5b7db979fc
miaosakurai <867616074@qq.com>
2022-01-22 15:41:31 +0900

    太累了叭

---
+++ 71. Simplify Path.py
@@ -0,0 +1,23 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        tokens = []
+        i,j = 0,0
+        while i<len(path):
+            if path[i]!='/':
+                j = i+1
+                while j<len(path) and path[j]!='/':
+                    j+=1
+                tokens.append(path[i:j])
+                i=j+1
+            else:
+                i+=1
+        for t in tokens:
+            if t=='.':
+                continue
+            elif t=='..':
+                if len(stack)>0:
+                    stack.pop()
+            else:
+                stack.append(t)
+        return '/'+"/".join(stack)
