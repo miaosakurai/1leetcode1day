e3c5a805ec39d931c9c77d20d3b396451d0fd32d
miaosakurai <867616074@qq.com>
2022-06-12 17:12:29 +0900

    周末要好好放松一下

--- 703. Kth Largest Element in a Stream.py
+++ 703. Kth Largest Element in a Stream.py
@@ -1,4 +1,5 @@
-class KthLargest:
+# 1. first solution
+class KthLargest1:
     def __init__(self, k: int, nums: List[int]):
         self.topk = sorted(nums, reverse=True)[:k]
         self.k = k
@@ -10,4 +11,29 @@ class KthLargest:
         self.topk.insert(i+1, val)
         self.topk = self.topk[:self.k]
         return self.topk[-1]
+
+
+# 2. priority queue, heap
+import heapq
+
+class KthLargest:
+    # O(n*logn)
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap) # O(n)
+        for i in range(len(self.heap)-k): # O(n*logn)
+            heapq.heappop(self.heap)
+
+    # O(logk)
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap) # O(logk)
+        return self.heap[0]
         
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
